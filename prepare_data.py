7f7a9db0afefed2672f0be8cea4a0d4d7b69c2ca
Cezary Szulc <cezary.szulc@gmail.com>
2017-10-18 17:37:08 +0200

    decrease datasets

--- prepare_data.py
+++ prepare_data.py
@@ -5,6 +5,7 @@ Created on Wed Oct 18 16:25:34 2017
 @author: C
 """
 from sklearn.cross_validation import train_test_split
+from sklearn.decomposition import PCA
 from numpy import corrcoef
 from pandas import DataFrame
 
@@ -17,12 +18,18 @@ class predict():
             data, target, train_size=0.8, random_state=43
         )
     
-    def show_raport(self):
-        print(corrcoef(self.X_train.T))
-        self.X_train.boxplot()
+    def show_raport(self, X):
+        print(corrcoef(X.T))
+        X = DataFrame(X)
+        X.boxplot()
         
+    def decrease_datasets(self):
+        pca = PCA()
+        x_train_tmp = pca.fit_transform(self.X_train)
+        self.show_raport(x_train_tmp)
         
 from sklearn.datasets import load_iris
 data = load_iris()
 iris_pr = predict(data.data, data.target)
-iris_pr.show_raport()
+iris_pr.show_raport(iris_pr.X_train)
+#iris_pr.decrease_datasets()
