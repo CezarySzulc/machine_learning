e45f39e2ff43b28c2db8503117c8085eae7cc952
Cezary Szulc <cezary.szulc@gmail.com>
2017-10-18 16:41:04 +0200

    split data

--- prepare_data.py
+++ prepare_data.py
@@ -4,9 +4,12 @@ Created on Wed Oct 18 16:25:34 2017
 
 @author: C
 """
-
+from sklearn.cross_validation import train_test_split
 
 
 class predict():
-    def __init__(data):
-        self.data = data
+    def __init__(self, data, target):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            data, target, train_size=0.8, random_state=43
+        )
+    
