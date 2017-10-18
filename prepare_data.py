c15da95fd2403e452b8b6997c1516d0884328143
Cezary Szulc <cezary.szulc@gmail.com>
2017-10-18 22:28:29 +0200

    add pipeline

--- prepare_data.py
+++ prepare_data.py
@@ -6,12 +6,17 @@ Created on Wed Oct 18 16:25:34 2017
 """
 from sklearn.cross_validation import train_test_split
 from sklearn.decomposition import PCA
-from numpy import corrcoef
+from sklearn.preprocessing import RobustScaler, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from numpy import corrcoef, arange
 from pandas import DataFrame
 
 
-class predict():
+class predict_clf():
     def __init__(self, data, target):
+        """ split data for training and test values """
         data = DataFrame(data)
         target = DataFrame(target)
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
@@ -19,17 +24,29 @@ class predict():
         )
     
     def show_raport(self, X):
+        """ show correlation in datasets and plot boxplot """
         print(corrcoef(X.T))
         X = DataFrame(X)
         X.boxplot()
         
-    def decrease_datasets(self):
+    def pipeline_k_neighbours(self):
         pca = PCA()
-        x_train_tmp = pca.fit_transform(self.X_train)
-        self.show_raport(x_train_tmp)
+        clf = KNeighborsClassifier(5)
+        # pipeline with pca and kneighbours
+        pipeline = Pipeline(steps=[('pca', pca), ('clf', clf)])
+        #best_k_param = {'clf__n': arange(0, 2, 0.1)}
+        #search_func = GridSearchCV(pipeline, best_k_param)
+        pipeline.fit(self.X_train, self.y_train)
+        return pipeline
+        
+    def test_classification(self, clf):
+        print(clf.score(self.X_test, self.y_test))
+
         
 from sklearn.datasets import load_iris
 data = load_iris()
-iris_pr = predict(data.data, data.target)
-iris_pr.show_raport(iris_pr.X_train)
+iris_pr = predict_clf(data.data, data.target)
+#iris_pr.show_raport(iris_pr.X_train)
 #iris_pr.decrease_datasets()
+clf = iris_pr.pipeline_k_neighbours()
+iris_pr.test_classification(clf)
